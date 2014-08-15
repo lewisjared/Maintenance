61fb688f7d15adcdce60e5bb42d12b4b71ea5bd7
Jared Lewis <jared@jared.kiwi.nz>
2014-08-15 21:30:50 +1200

    Removed some unnecessary css

--- Maintenance/App_Start/BundleConfig.cs
+++ Maintenance/App_Start/BundleConfig.cs
@@ -24,8 +24,7 @@ namespace Maintenance
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-			bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css",
-				"~/Content/stylish-portfolio.css"));
+			bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
 
